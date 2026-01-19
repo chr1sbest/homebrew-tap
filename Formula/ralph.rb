9918435d89de888e34b7ce9f208f3f03cb23c0ef
Chris Best <chr2sbest@gmail.com>
2026-01-19 15:16:38 -0500

    Update ralph to v1.1.0

--- Formula/ralph.rb
+++ Formula/ralph.rb
@@ -1,25 +1,25 @@
 class Ralph < Formula
   desc "Ralph Loop (Claude Code) with some bells and whistles"
   homepage "https://github.com/chr1sbest/wiggum"
-  version "1.0.0"
+  version "1.1.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/chr1sbest/wiggum/releases/download/v1.0.0/ralph_darwin_arm64.tar.gz"
-      sha256 "f4aa6dfc345cf4f8175ea15173b8caa6b20079014450ec2e1c01e3bf66ed6913"
+      url "https://github.com/chr1sbest/wiggum/releases/download/v1.1.0/ralph_darwin_arm64.tar.gz"
+      sha256 "93c5410ce2be72256ecf6f9775491fc3c2048b60f579d5034dcdc8dffd1b1656"
     else
-      url "https://github.com/chr1sbest/wiggum/releases/download/v1.0.0/ralph_darwin_amd64.tar.gz"
-      sha256 "25b8dc89850bce958620880d562f226a6b161dde766f541f43882e68e0c829c7"
+      url "https://github.com/chr1sbest/wiggum/releases/download/v1.1.0/ralph_darwin_amd64.tar.gz"
+      sha256 "9a7bdfb7931d70384f08fa2a5286b92f7d761bf3d45c55b44b18e17bd61bad14"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/chr1sbest/wiggum/releases/download/v1.0.0/ralph_linux_arm64.tar.gz"
-      sha256 "cd53368cbd313af08abd6af1c4e8b1fc32ae6bfaa0da7fa5e0016ab7423b9cd4"
+      url "https://github.com/chr1sbest/wiggum/releases/download/v1.1.0/ralph_linux_arm64.tar.gz"
+      sha256 "def29792968e7131ec144abd01bdf4e4ec83c9d6b70e277beef572da0410f493"
     else
-      url "https://github.com/chr1sbest/wiggum/releases/download/v1.0.0/ralph_linux_amd64.tar.gz"
-      sha256 "2d9f8d675c673c8bc61dcc2355587b33fea515c73d0ddad90272ba0967c78d4a"
+      url "https://github.com/chr1sbest/wiggum/releases/download/v1.1.0/ralph_linux_amd64.tar.gz"
+      sha256 "af0c37a07bb0d7366c83686f958eeeea905ffeee37dc4ea005f6f2f9fa0ebd36"
     end
   end
 
